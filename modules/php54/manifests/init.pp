4c839c976fb99d5efdb02a44ea4723274c0c3207
Ryan Neufeld <ryan@neufeldmail.com>
2012-08-29 17:01:23 -0700

    adding in apt module and dependencies

--- modules/php54/manifests/init.pp
+++ modules/php54/manifests/init.pp
@@ -1,23 +1,19 @@
 class php54 {
-	file { "/etc/apt/sources.list":
-		ensure => file,
-		owner  => root,
-		group  => root,
-		source => "puppet:///modules/php54/squeeze.sources.list",
+	apt::source {'php54_dotdeb':
+		location    =>"http://packages.dotdeb.org/",
+		release     => "squeeze-php54",
+		repos       => "all",
+		include_src => false,
 	}
 
 	exec { "import-gpg":
 		command => "/usr/bin/wget -q http://www.dotdeb.org/dotdeb.gpg -O -| /usr/bin/apt-key add -"
 	}
 
-	exec { "/usr/bin/apt-get update":
-		require => [File["/etc/apt/sources.list"], Exec["import-gpg"]],
-	}
-
 	package { [
 		"php5"
 	] :
 		ensure => latest,
-		require => Exec["/usr/bin/apt-get update"],
+		require => Exec["apt_update"],
 	}
 }
