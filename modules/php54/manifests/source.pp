c9acb21a360f620088a6829ca0506af8c8e73bd0
Ryan Neufeld <ryan@neucode.org>
2012-09-02 13:30:32 -0700

    Adding php54 source

---
+++ modules/php54/manifests/source.pp
@@ -0,0 +1,20 @@
+class php54::source {
+  case $operatingsystem {
+    debian: {
+      apt::source {'php54_dotdeb':
+        ensure      => present,
+        location    => "http://packages.dotdeb.org/",
+        release     => "squeeze-php54",
+        repos       => "all",
+        include_src => false,
+        key         => "3D624A3B",
+        key_source  => "http://www.dotdeb.org/dotdeb.gpg"
+      }
+    }
+    ubuntu: {
+      apt::ppa { "ppa:ondrej/php5": }
+    }
+
+    default: { fail("Unrecognized operating system for webserver")}
+  }
+}
