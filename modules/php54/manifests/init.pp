eb7e17a1ec05c183c43d72b0f085b5fea7959411
Ryan Neufeld <ryan@neufeldmail.com>
2012-09-06 17:00:02 -0700

    Adding memcache, xmlrpc, cli, and apache trigger.

--- modules/php54/manifests/init.pp
+++ modules/php54/manifests/init.pp
@@ -3,8 +3,13 @@ class php54 {
 
   package {[
   	  "php5",
+      "php5-cli",
   	  "php5-mysqlnd",
+      "php5-memcache",
+      "php5-memcached",
   	  "php5-mcrypt",
+      "php5-xmlrpc",
+      "php5-curl",
   	  "php5-gd",
   	  "libphp-phpmailer",
   	  "php-gettext",
@@ -12,6 +17,7 @@ class php54 {
   	] :
     ensure  => latest,
     require => [Class['php54::source']],
+    notify  => Service["apache2"],  # this sets up the relationship
   }
 }
 
