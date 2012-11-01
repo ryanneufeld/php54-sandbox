a5632807a0b07fd0c6fb2817550ce61501ee8c86
Ryan Neufeld <ryan@neucode.org>
2012-11-01 09:01:09 -0700

    adding mysql config

--- modules/devsite/manifests/init.pp
+++ modules/devsite/manifests/init.pp
@@ -17,11 +17,25 @@ class devsite {
   apache::mod {"cache":}
   apache::mod {"expires":}
   apache::mod {"actions":}
+
   include apache::mod::php
   include devsite::phpconfig
 
   Class["php54"] ->
   Class["Apache::Mod::Php"] ->
   Class["devsite::phpconfig"]
+
+  class { 'mysql::server':
+    config_hash => { 'root_password' => 'vagabond' }
+  }
+
+  database { 'information_schema':
+    ensure  => 'present',
+    charset => 'utf8',
+  }
+  database { 'mysql':
+    ensure  => 'present',
+    charset => 'utf8',
+  }
 
 }
